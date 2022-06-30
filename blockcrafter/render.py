6805b02d345f811529fd4d8b3c1dd5cd0a035bea
Michael <michaellavaire@gmail.com>
2022-06-30 15:01:22 -0400

    Implement rescale attribute (Fix size of rails, grass, flowers, sugar cane, ...)

--- blockcrafter/render.py
+++ blockcrafter/render.py
@@ -374,6 +374,17 @@ class Element:
                     "z" : [0, 0, 1]}[rotationdef["axis"]]
             origin = (np.array(rotationdef.get("origin", [8, 8, 8]), dtype=np.float32) - 8.0) / 16.0 * 2.0
             element_rotation = np.dot(transforms.translate(-origin), np.dot(transforms.rotate(rotationdef["angle"], axis), transforms.translate(origin)))
+            if (rotationdef.get("rescale", False)):
+                rescaleaxis = {
+                    "x" : [0, 1, 1, 0],
+                    "y" : [1, 0, 1, 0],
+                    "z" : [1, 1, 0, 0]}[rotationdef["axis"]]
+                if (abs(rotationdef["angle"]) == 22.5):
+                    rescale = np.array(rescaleaxis, dtype=np.float32) * abs((1.0 / math.cos(math.radians(22.5)) - 1.0))
+                else:
+                    rescale = np.array(rescaleaxis, dtype=np.float32) * abs((1.0 / math.cos(math.radians(45)) - 1.0))
+                rescale = 1 + rescale
+                element_rotation = rescale * element_rotation
 
         program = Element.get_program(mode)
         self.current_program = program
